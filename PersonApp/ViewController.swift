8325ccff4cef7d46da51ecdeacdf8a5ede92e956
Denis <majestycrew@gmail.com>
2022-05-28 14:10:16 +0300

    Initial Commit

---
+++ PersonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonApp
+//
+//  Created by Denis Shpagin on 28.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
